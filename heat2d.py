5b76033b7aaea36749cb069c7ba3ab78faef7bb6
Gustavo ANJOS <gustavo.rabello@gmail.com>
2007-06-12 04:37:16 +0000

    inclusao de PRINT em alguns casos e inicio do desenvolvimento do metodo eulerImplicito

--- heat2d.py
+++ heat2d.py
@@ -27,6 +27,32 @@ def condicaoContorno(f,L,nx,ny):
   fnew[i][nx-1] = f[ny-1][0] + i*L/(ny-1) * i*L/(ny-1)
  return f
 
+def eulerImplicito(nx,ny,dt,k,dx,dy,f):
+ K = zeros( ((nx-2)*(nx-2),(ny-2)*(ny-2)),double )
+
+ # loop dos f[i][j] para K
+ for i in range( 0, (nx-2)*(nx-2) ):
+   K[i][i] = 1 + 2*(dt*k)/(dx*dx) + 2*(dt*k)/(dy*dy)
+ 
+ # loop dos f[i+1][j] para K
+ for i in range( 0, (nx-2)*(nx-2)-1 ):
+   K[i+1][i] = -(dt*k)/(dx*dx)
+   
+ # loop dos f[i-1][j] para K
+ for i in range( 1, (nx-2)*(nx-2) ):
+   K[i-1][i] = -(dt*k)/(dx*dx)
+   
+ # loop dos f[i][j+1] para K
+ for j in range( 0, (ny-2)*(ny-2)-3 ):
+  K[j+3][j] = -(dt*k)/(dy*dy)
+   
+ # loop dos f[i][j-1] para K
+ for j in range( 3, (ny-2)*(ny-2) ):
+  K[j-3][j] = -(dt*k)/(dy*dy)
+   
+ print K
+ return K
+
 def eulerExplicito(nx,ny,dt,k,dx,dy,f):
  for i in range( 1,nx-1 ): # loop sobre a malha
   for j in range( 1,ny-1 ):
@@ -39,19 +65,6 @@ def eulerExplicito(nx,ny,dt,k,dx,dy,f):
    varY = ( a*f[i][j-1] + b*f[i][j] + c*f[i][j+1] )
    fnew[i][j]=f[i][j] + cteX * varX + cteY * varY 
  return fnew
-
-def eulerImplicito(nx,ny,dt,k,dx,dy,f):
- K = zeros( (nx,ny),double )
- for i in range( 1,nx-1 ):
-  for j in range( 1,ny-1 ):
-   cteX=(dt*k)/(dx*dx)
-   cteY=(dt*k)/(dy*dy)
-   a = -cteX
-   b = (1+2*(cteX+cteY))
-   c = -cteY
-   K[i][j] = a*(fnew[i-1][j]+fnew[i+1][j]) + b*fnew[i][j] + c*(fnew[i][j-1]+fnew[i][j+1])
-   print K
- return K
 
 def residuo(f,fnew,nx,ny):
  residuo = 0.0
@@ -75,7 +88,7 @@ def vtkOut(X,Y,f,nx,ny):
  vtkFile.write( "DIMENSIONS %d %d %d\n" %(nx,ny,1) )
  vtkFile.write( "POINTS %d float\n" %X.size )
 
- fOut = zeros( (nx*ny),float )
+ #fOut = zeros( (nx*ny),float )
  fOut = reshape( f,(nx*ny) ) 
  for i in range( 0,nx*ny ):
   vtkFile.write( "%f %f %f\n" %(X[i],Y[i],X[i]*0) )
@@ -91,13 +104,14 @@ def vtkOut(X,Y,f,nx,ny):
 
 # Execucao do programa
 
-nx  = 3     # numero de pontos em x
-ny  = 3     # numero de pontos em y
+nx  = 20     # numero de pontos em x
+ny  = 20     # numero de pontos em y
 L   = 1.0   # comprimento total
 dx  = L/nx  # intervalo dx
 dy  = L/ny  # intervalo dy
-dt  = 0.002  # intervalo de tempo
+dt  = 0.0002  # intervalo de tempo
 k   = 1
+nIterMax = 5200
 EPS = 1.0E-5 # precisao
 
 f    = zeros( (nx,ny),double );
@@ -107,16 +121,25 @@ Y    = zeros( (nx*ny),double );
 
 malha(L,nx,ny,X,Y)
 f = condicaoContorno(f,L,nx,ny)
-for i in range(1,1000):
+for i in range(1,nIterMax+1):
  fnew = eulerExplicito(nx,ny,dt,k,dx,dy,f)
  erro = residuo(f,fnew,nx,ny)
+
  if( erro < EPS ):
+  print( "Convergiu para uma solucao com erro < %g" %EPS )
+  print( "erro = %f" %erro )
+  print( "iteracao = %d" %i )
   break
+ if( erro > 1.0e+1 ):
+  print( "solucao divergiu! Voce precisa ajustar relacao malha e dt!" )
+  break
+ if( i == nIterMax ):
+  print( "a solucao chegou ao maximo de iteracoes sem convergir" )
+  print( "erro = %f" %erro )
+  print( "iteracao = %d" %(nIterMax) )
+
  f    = atualizaF(nx,ny,fnew)
 
-print( "Convergiu para uma solucao com erro < 10e-05" )
-print( "erro = %f" %erro )
-print( "iteracao = %d" %i )
 
 vtkOut(X,Y,f,nx,ny)
 
